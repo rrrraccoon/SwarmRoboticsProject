7c8d28a794a6ea1b996df43c89a225e90c4cc625
rrrraccoon <sidraaj0401@gmail.com>
2026-04-24 22:10:38 +0100

    Update Obstacles.py

--- SwarmRobotics/Obstacles.py
+++ SwarmRobotics/Obstacles.py
@@ -10,17 +10,25 @@ class Obstacles:
 
         #create obstacles based on option user chose (1 or 2)
         if option == 1:
-            self.obstacles = [pygame.Rect(600, 150, 30, 30),
+            self.obstacles = [
                          pygame.Rect(400, 300, 30, 30),
-                         pygame.Rect(550, 500, 30, 30),
+                         pygame.Rect(550, 500, 60, 30),
                          pygame.Rect(550, 400, 30, 30),
                          pygame.Rect(550, 200, 60, 30),
+                         pygame.Rect(330, 200, 30, 30),
+                         pygame.Rect(350, 100, 30, 30),
+                         pygame.Rect(650, 150, 30, 30),
+                         pygame.Rect(800, 500, 30, 30),
                          pygame.Rect(800, 250, 30, 30)]
 
         else:
-            self.obstacles = [pygame.Rect(600, 150, 30, 30),
+            self.obstacles = [
+                         pygame.Rect(600, 150, 30, 30),
                          pygame.Rect(400, 300, 30, 30),
                          pygame.Rect(550, 500, 30, 30),
+                         pygame.Rect(700, 500, 60, 30),
+                         pygame.Rect(700, 300, 30, 30),
+                         pygame.Rect(350, 200, 30, 30),
                          pygame.Rect(800, 250, 30, 30)]
 
 
@@ -44,11 +52,11 @@ class Obstacles:
 
     def collision_robot(self, robot_x, robot_y, robot_radius):
         for obstacle in self.obstacles:
-            # Find the closest point on the rect to the robot's centre
+            #closest point on the rect to the robots centre
             closest_x = max(obstacle.left, min(robot_x, obstacle.right))
             closest_y = max(obstacle.top, min(robot_y, obstacle.bottom))
 
-            # If the distance from that point to the centre is less than the radius, they overlap
+            #if the distance from that point to the centre is less than the radius, they overlap
             distance = math.sqrt((robot_x - closest_x) ** 2 + (robot_y - closest_y) ** 2)
             if distance < robot_radius:
                 return True
